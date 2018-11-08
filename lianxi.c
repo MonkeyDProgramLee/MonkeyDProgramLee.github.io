6209b382c08c4e6cf8d8edd4145a8bf9000444ce
MonkeyDProgramLee <44746069+MonkeyDProgramLee@users.noreply.github.com>
2018-11-08 23:57:37 +0800

    Add files via upload

---
+++ lianxi.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int calobj(int n,int i){
+	int sum;
+	for (int a = 0;a<=)
+
+
+}
+int main(){
+	//输出一个a，b,c中最大的；
+	/*int a = 5, b = 3, c = 7;
+	if (a > b){
+		;
+	}
+	else	{
+		int max = a;
+		a = b;
+		b = max;
+	}
+	if (a > c){
+		printf("%d",a);
+	}
+	else{
+		int max;
+		max = c;
+		c = a;
+		a = max;
+		printf("%d\n", a);
+	}
+	if (b > c){
+		printf("%d\n", b);
+		printf("%d\n", c);
+	}
+	else{
+		printf("%d\n", c);
+		printf("%d\n", b);
+	}*/
+
+	/*printf("*************\n");
+	printf("Very good!\n");
+	printf("*************\n");
+	return 0;*/
+
+}
