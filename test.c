3d5b751e4c680abed01c33fc821cc9bce6c666a6
MonkeyDProgramLee <44746069+MonkeyDProgramLee@users.noreply.github.com>
2018-11-12 23:51:49 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,5 +1,3 @@
-//玩家落子判断游戏是否结束
-//电脑落子判断游戏是否结束
 #define  _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
@@ -8,19 +6,21 @@
 #define COL 3
 char g_checkerboard[ROW][COL];
 void Init(){
-		int row, col;
-		for (row = 0; row < ROW; ++row){
-			for (col = 0; col < COL; ++col){
-				g_checkerboard[row][col] = 'o';
-			}
+	int row, col;
+	for (row = 0; row < ROW; ++row){
+		for (col = 0; col < COL; ++col){
+			g_checkerboard[row][col] = ' ';
 		}
 	}
+}
 void Print(){
 	for (int row = 0; row < ROW; ++row){
 		if (row == 0){
 			printf(" -----------\n");
 		}
-		printf("| %c | %c | %c |\n", g_checkerboard[row][0], g_checkerboard[row][1],
+		printf("| %c | %c | %c |\n", 
+			g_checkerboard[row][0],
+			g_checkerboard[row][1],
 			g_checkerboard[row][2]);
 		printf(" -----------\n");
 	}
@@ -30,108 +30,118 @@ void Playermove(){
 	while (1){
 		printf("请输入下子的坐标：\n");
 		scanf("%d,%d", &row, &col);
-		g_checkerboard[row][col]='o';
-		if (row<0 || row>3 || col<0 || col>3||g_checkerboard[row][col]!=' '){
+		if (row<0 || row>=ROW || col<0 || col>=COL ||
+			g_checkerboard[row][col] != ' '){
 			printf("输入有误请再次输入");
 			continue;
 		}
-		break;
-	}
-}
-void Computermove(){
-	int col, row;
-	srand((unsigned int)time(0));
-	while (1)
-	{
-		col = rand() % 3;
-		row = rand() % 3;
-		g_checkerboard[row][col] = 'x';
-		if (row<0 || row>3 || col<0 || col>3 || g_checkerboard[row][col] != ' '){
-			printf("输入有误请再次输入");
-			continue;
+		else{
+			g_checkerboard[row][col] = 'o';
+			break;
 		}
 	}
 }
 char Checkwinner(){
-	//如果对角线行列值都为o则判plyer为赢
-	int col, row,count=0;
+	int col, row, count = 0;
+	//玩家赢
 	//1.判断行
 	for (row = 0; row < ROW; ++row){
-		if (g_checkerboard[row][0] = 'o' == g_checkerboard[row][1] ==
-			g_checkerboard[row][2])
+		if (g_checkerboard[row][0] == 'o'&& g_checkerboard[row][1] == 'o'&&
+			g_checkerboard[row][2]=='o')
 			return 'o';
 	}
 	//2.判断列
-	for (col = 0; col < COL; ++col){
-		if (g_checkerboard[0][col] = 'o' == g_checkerboard[1][col] ==
-			g_checkerboard[2][col])
+	for (col = 0; col < col; ++col){
+		if (g_checkerboard[0][col] =='o' &&
+			g_checkerboard[1][col] == 'o'&&
+			g_checkerboard[2][col]=='o')
 			return 'o';
 	}
-		//3.对角线
-		if (g_checkerboard[0][0] == g_checkerboard[1][1] == 
-			g_checkerboard[2][2]=='o'|| 
-			g_checkerboard[0][2] ==g_checkerboard[1][1] == 
-			g_checkerboard[2][0]=='o')
-			return 'o';
-			//computer
-			for (row = 0; row < ROW; ++row){
-				if (g_checkerboard[row][0] = 'x' == g_checkerboard[row][1] ==
-					g_checkerboard[row][2])
-					return 'o';
+	//3.判断对角线
+	if (g_checkerboard[0][0] == 'o'&& g_checkerboard[1][1] == 'o'&&
+		g_checkerboard[2][2] == 'o' ||
+		g_checkerboard[0][2] == 'o'&& g_checkerboard[1][1] == 'o'&&
+		g_checkerboard[2][0] == 'o')
+		return 'o';
+	//电脑赢
+	////1.判断行
+	for (row = 0; row < ROW; ++row){
+		if (g_checkerboard[row][0] =='x' && g_checkerboard[row][1] == 'x'&&
+			g_checkerboard[row][2]=='x')
+			return 'x';
+	}
+	////判断列
+	for (col = 0; col < COL; ++col){
+		if (g_checkerboard[0][col] == 'x'&&g_checkerboard[1][col] == 'x'
+			&&g_checkerboard[2][col]=='x')
+			return 'x';
+	}
+	//判断对角线
+	if (g_checkerboard[0][0] = 'x'&& g_checkerboard[1][1] == 'x'&&
+		g_checkerboard[2][2] == 'x' ||
+		g_checkerboard[0][2] == 'x'&&g_checkerboard[1][1] == 'x'&&
+		g_checkerboard[2][0] == 'x')
+		return 'x';
+	////判断平局
+	for (row = 0; row < ROW; ++row){
+		for (col = 0; col < COL; ++col){
+			if (g_checkerboard[row][col] == ' ')
+				break;
+			else
+				count++;
+		}
+	}
+	if (count == COL*ROW)
+		return '_';
+}
+void Computermove(){
+	
+	while (1){
+		int col, row;
+		col = rand() % 3;
+		row = rand() % 3;
+		if (g_checkerboard[row][col] == ' '){
+			g_checkerboard[row][col] = 'x';
+			break;
+		}
+		continue;
+	}
+}
+int main(){
+	srand((unsigned int)time(0));
+	char winner;
+		//绘制棋盘
+		//1.初始化数组
+		Init();
+		//2.绘制棋盘
+		while (1){
+			Print();
+			//玩家落子
+			Playermove();
+			//判断是否赢了
+			winner = Checkwinner();
+			printf("canshu%c\n", winner);
+			if (winner == 'o'){
+				Print();
+				printf("玩家赢了\n");
+				break;
 			}
-			for (col = 0; col < COL; ++col){
-				if (g_checkerboard[0][col] = 'x' == g_checkerboard[1][col] ==
-					g_checkerboard[2][col])
-					return 'o';
+			if (winner == '_'){
+				printf("平局");
+				break;
 			}
-				if (g_checkerboard[0][0] == g_checkerboard[1][1] ==
-					g_checkerboard[2][2] == 'x' ||
-					g_checkerboard[0][2] == g_checkerboard[1][1] ==
-					g_checkerboard[2][0] == 'x'){
-					return 'o';
-					for (row = 0; row < ROW; ++row){
-						for (col = 0; col < COL; ++col){
-							if (g_checkerboard[row][col] == ' ')
-								count++;
-						}
-						if (count == 0)
-							return '_';
-
-					}
-				
-
-					int main() {
-						Init();
-						Print();
-						char winner;
-						while (1){
-							Playermove();
-							winner = Checkwinner();
-							if (winner = 'x'){
-								printf("玩家胜利");
-								break;
-							}
-							else if (winner == 'o'){
-								printf("电脑胜利");
-								break;
-							}
-							else if (winner == ' ')
-							{
-								printf("平局");
-							}
-							Computermove();
-							if (winner = 'x'){
-								printf("玩家胜利");
-								break;
-							}
-							else if ( winner == 'o'){
-								printf("电脑胜利");
-								break;
-							}
-							else if (winner == ' ')
-							{
-								printf("平局");
-							}
-						}
-					}
-			
+			//电脑赢了
+			Computermove();
+			winner = Checkwinner();
+			printf("canshu%c\n", winner);
+			if (winner =='x'){
+				Print();
+				printf("电脑获得胜利\n");
+				break;
+			}
+			if (winner == '_'){
+				printf("平局");
+				break;
+			}
+		}
+}
