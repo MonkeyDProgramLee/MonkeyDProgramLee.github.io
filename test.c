76c92734963fd8306d6630a2466ba848310467f2
MonkeyDProgramLee <44746069+MonkeyDProgramLee@users.noreply.github.com>
2018-11-15 23:25:44 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,58 +1,146 @@
+#define  _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-void swp(int* a, int* b){
-	int temp;
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-void swp2(char* a, char* b){
-	char temp;
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-void paosort(int arr[], int size){
-	int boundary;
-	for (boundary = size; boundary > 0; --boundary){
-		for (int i = 0; i < boundary; ++i){
-			if (arr[boundary]>arr[i]){
-				swp(&arr[boundary], &arr[i]);
-			}
+#include<time.h>
+#define ROW 3
+#define COL 3
+char g_checkerboard[ROW][COL];
+void Init(){
+	int row, col;
+	for (row = 0; row < ROW; ++row){
+		for (col = 0; col < COL; ++col){
+			g_checkerboard[row][col] = ' ';
 		}
+	}
+}
+void Print(){
+	for (int row = 0; row < ROW; ++row){
+		if (row == 0){
+			printf(" -----------\n");
+		}
+		printf("| %c | %c | %c |\n", 
+			g_checkerboard[row][0],
+			g_checkerboard[row][1],
+			g_checkerboard[row][2]);
+		printf(" -----------\n");
+	}
+}
+void Playermove(){
+	int row, col;
+	while (1){
+		printf("请输入下子的坐标：\n");
+		scanf("%d,%d", &row, &col);
+		if (row<0 || row>=ROW || col<0 || col>=COL ||
+			g_checkerboard[row][col] != ' '){
+			printf("输入有误请再次输入");
+			continue;
+		}
+		else{
+			g_checkerboard[row][col] = 'o';
+			break;
+		}
+	}
+}
+char Checkwinner(){
+	int col, row, count = 0;
+	//玩家赢
+	//1.判断行
+	for (row = 0; row < ROW; ++row){
+		if (g_checkerboard[row][0] == 'o'&& g_checkerboard[row][1] == 'o'&&
+			g_checkerboard[row][2]=='o')
+			return 'o';
+	}
+	//2.判断列
+	for (col = 0; col < col; ++col){
+		if (g_checkerboard[0][col] =='o' &&
+			g_checkerboard[1][col] == 'o'&&
+			g_checkerboard[2][col]=='o')
+			return 'o';
+	}
+	//3.判断对角线
+	if (g_checkerboard[0][0] == 'o'&& g_checkerboard[1][1] == 'o'&&
+		g_checkerboard[2][2] == 'o' ||
+		g_checkerboard[0][2] == 'o'&& g_checkerboard[1][1] == 'o'&&
+		g_checkerboard[2][0] == 'o')
+		return 'o';
+	//电脑赢
+	////1.判断行
+	for (row = 0; row < ROW; ++row){
+		if (g_checkerboard[row][0] =='x' && g_checkerboard[row][1] == 'x'&&
+			g_checkerboard[row][2]=='x')
+			return 'x';
+	}
+	////判断列
+	for (col = 0; col < COL; ++col){
+		if (g_checkerboard[0][col] == 'x'&&g_checkerboard[1][col] == 'x'
+			&&g_checkerboard[2][col]=='x')
+			return 'x';
+	}
+	//判断对角线
+	if (g_checkerboard[0][0] =='x'&& g_checkerboard[1][1] == 'x'&&
+		g_checkerboard[2][2] == 'x' ||
+		g_checkerboard[0][2] == 'x'&&g_checkerboard[1][1] == 'x'&&
+		g_checkerboard[2][0] == 'x')
+		return 'x';
+	////判断平局
+	for (row = 0; row < ROW; ++row){
+		for (col = 0; col < COL; ++col){
+			if (g_checkerboard[row][col] == ' ')
+				break;
+			else
+				count++;
+		}
+	}
+	if (count == COL*ROW)
+		return '_';
+}
+void Computermove(){
+	
+	while (1){
+		int col, row;
+		col = rand() % 3;
+		row = rand() % 3;
+		if (g_checkerboard[row][col] == ' '){
+			g_checkerboard[row][col] = 'x';
+			break;
+		}
+		continue;
 	}
 }
 int main(){
- //   int	arr[] = { 1, 5, 2, 4, 7 };
-	//int boundary;
-	//int len = sizeof(arr) / sizeof(arr[0]);
-	//boundary = len - 1;//数组的长度为边界
-	//for (; boundary > 0; --boundary){
-	//	for (int i = 0; i < boundary; ++i){
-	//		if (arr[boundary] < arr[i]){
-	//			swp(&arr[boundary],&arr[i]);
-	//		}
-	//	}
-	//}
-	//paosort(&arr[0], len);
-	//for (int i = 0; i < len; ++i){
-	//	printf("%d\n",arr[i]);
-	//}
-	char str1[] = "hello world";
-	char str2[] = "###########";
-	int left = 0;
-	int lenth = sizeof(str1) / sizeof(str2[0]);
-	int right = lenth - 1;
-	while (left<right)
-	{
-		swp2(&str1[left], &str2[left]);
-		swp2(&str1[right], &str2[right]);
-		for (int i = 0; i < lenth; ++i){
-			printf("%s\n", str1[i]);
+	srand((unsigned int)time(0));
+	char winner;
+		//绘制棋盘
+		//1.初始化数组
+		Init();
+		//2.绘制棋盘
+		while (1){
+			Print();
+			//玩家落子
+			Playermove();
+			//判断是否赢了
+			winner = Checkwinner();
+			printf("canshu%c\n", winner);
+			if (winner == 'o'){
+				Print();
+				printf("玩家赢了\n");
+				break;
+			}
+			if (winner == '_'){
+				printf("平局");
+				break;
+			}
+			//电脑赢了
+			Computermove();
+			winner = Checkwinner();
+			if (winner =='x'){
+				Print();
+				printf("电脑获得胜利\n");
+				break;
+			}
+			if (winner == '_'){
+				printf("平局");
+				break;
+			}
 		}
-		left++;
-		right--;
-	}
-	
 }
