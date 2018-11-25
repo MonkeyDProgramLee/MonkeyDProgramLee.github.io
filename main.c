f2ba15f5bcf29e1c9238f95a3e0dcb23ca648c39
MonkeyDProgramLee <44746069+MonkeyDProgramLee@users.noreply.github.com>
2018-11-25 23:10:56 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct list {
+	char goods[1024];
+	int  count;
+}List;
+typedef struct Goods {
+	int Id;
+	double price;
+	int sum;
+	int left;
+	int sell;
+}Goods;
+typedef struct customer {
+	char name[1024];
+	int age;
+	int VIPID;
+}customer;
+int main() {
+
+}
