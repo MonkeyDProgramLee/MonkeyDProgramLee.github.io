3b5f46afadcb02502fff66a3b8add1290fd02dc2
MonkeyDProgramLee <44746069+MonkeyDProgramLee@users.noreply.github.com>
2018-12-02 23:09:12 +0800

    Add files via upload

--- List.h
+++ List.h
@@ -1,91 +1,63 @@
 #pragma once
+#include<stdio.h>
 #include<stdlib.h>
-#include<assert.h>
-typedef int DataType;
-typedef struct ListNode {
-	DataType data;
-	struct ListNode* next;
-}ListNode;
-//初始化
-//目的是让这个链表为空链表操作头指针 要修改他的值为NULL 所以需要头结点的地址指针的地址所以传入二级指针
-void Init(ListNode **ppFirst) {
-	assert(ppFirst != NULL);
+typedef struct PCB {
+	//进程标志符
+	int order;
+	//预计运行时间
+	double pre_time;
+	//到达时间
+	double arrive_time;
+	//优先级
+	int super;
+	//开始时间
+	double start_time;
+	//完成时间
+	double finsh_time;
+	//周转时间
+	double zhouzhuan_time;
+	//带权周转时间
+	double daiquanzhouzhuan_time;
+	//平均周转时间
+	double pingjunzhouzhuan_time;
+	//平均带权周转时间
+	double pingjundaiquanzhouzhuan_time;
+}PCB;
+typedef struct PCBListNode {
+	PCB PBCNode;
+	struct PCBListNode* next;
+}PCBListNode;
+//初始化结点
+void Init(PCBListNode **ppFirst) {
 	*ppFirst = NULL;
 }
-static ListNode* CreatNode(DataType data) {
-	ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
-	newNode->data = data;
-	return newNode;
+//开创一个结点
+static PCBListNode* CreatPBCNode(PCB PBCNodedata) {
+	//开创空间
+	PCBListNode *NewNode = (PCBListNode*)malloc(sizeof(PCBListNode));
+	NewNode->PBCNode = PBCNodedata;
 }
-void ListNodePushFirst(ListNode** ppFirst, DataType data) {
-	ListNode* newNode = CreatNode(data);
-	newNode->next = *ppFirst; 
-	*ppFirst = newNode;
+//增
+void PushNodeFront(PCBListNode **ppFirst , PCB PBCNodedata) {
+	PCBListNode *NewNode = CreatPBCNode(PBCNodedata);
+	NewNode->next=*ppFirst;
+	*ppFirst = NewNode;
 }
-void ListNodePushBack(ListNode** ppfirst,DataType data){
-	//如果尾插的是空链表
-	if (*ppfirst == NULL) {
-		ListNode *newNode;
-		newNode = CreatNode(data);
-		newNode->next = *ppfirst;
-		*ppfirst = newNode;
-	}
-	ListNode *cur = *ppfirst;
-	while (cur->next != NULL) {
-		cur = cur->next;
-	}
-	ListNode *newNode;
-	newNode = CreatNode(data);
-	newNode->next = NULL;
-	cur->next = newNode;
-
+void Input(PCB *pcb) {
+	//输入进程标志符
+	int order;
+	printf("输入进程序号:");
+		scanf("%d", &order);
+	pcb->order = order;
 }
-//尾删
-//1.当节点数大于二时找倒数第二个结点
-//2.空链表无法删除
-//咋找倒数第二个结点
-//判断是不是空链表
-//判断是不是只有一个结点
-void ListPopBack(ListNode **ppfirst) {
-	assert(*ppfirst != NULL);
-	ListNode* cur=*ppfirst;
-		while (cur->next != NULL) {
-			cur = cur->next;
-		}
-		if ((*ppfirst)->next!=NULL) {
-			free(cur->next);
-			cur->next = NULL;
-		}
-		else {
-			free(cur->next);
-			*ppfirst = NULL;
-		}
+void Print(PCBListNode* pFirst) {
+	printf("内部标识符:%d\n", pFirst->PBCNode.order);
 }
-//查找
-ListNode *FindNode(ListNode* first,DataType to_find) {
-	//遍历
-	for (ListNode *cur = first; cur != NULL; cur = cur->next) {
-		if (to_find == cur->data) {
-			return cur;
-		}
-		else
-			return NULL;
-	}
+void  Test() {
+	PCB pcb;
+	PCBListNode* ppFirst;
+	Init(&ppFirst);
+	Input(&pcb);
+	PushNodeFront(&ppFirst, pcb);
+	Print(ppFirst);
 }
-//结点前插入
-void  ListInsert(ListNode **ppfirst, ListNode *pos, DataType data) {
-	//头插
-	if (*ppfirst == pos) {
-		ListNodePushFirst(ppfirst, data);
-		return;
-	}
-	//找pos的前一个结点
-	ListNode *cur = *ppfirst;
-	while (cur->next == pos) {
-		cur = cur->next;
-	}
-	ListNode *newnode = CreatNode(data);
-	newnode->next = cur->next;
-	cur->next = newnode;
-}
-//删除指定结点
